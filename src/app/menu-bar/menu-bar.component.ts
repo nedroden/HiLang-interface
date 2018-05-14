7faaffc11a25520bfc0e0e0f527aafd0b586dd36
TheElmo <jelmer.internet@xs4all.nl>
2018-05-14 13:31:16 +0200

    menuBar com seperated

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -10,6 +10,32 @@ export class MenuBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	this.createElement();
   }
+
+  createElement() {
+		let menuItems = [
+			{
+				label: "Home",
+			},
+			{
+				label: "Account",
+			},
+			{
+				label: "Browse",
+			},
+			{
+				label: "My courses",
+			},
+			{
+				label: "Log out",
+			},
+		];
+
+		for(let item of menuItems) {
+			let li-element = document.createElement("li");
+			element.innerText = item.label;
+			document.getElementById('menuBar').appendChild(element);
+		}
 
 }
