fadddb37513e93709b0046abc7679fec77724531
Robert Monden <dev@robertmonden.com>
2018-05-14 13:22:30 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GrammarComponent } from './exercises/grammar/grammar.component';
+import { FlashcardsComponent } from './exercises/flashcards/flashcards.component';
+import { LessonviewComponent } from './lessonview/lessonview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrammarComponent,
+    FlashcardsComponent,
+    LessonviewComponent
   ],
   imports: [
     BrowserModule
