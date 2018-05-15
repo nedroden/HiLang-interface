a61528ac065b4d7a6e23c11de58f325825a469a4
jhhaanstra <janhendrikhaanstra@gmail.com>
2018-05-15 09:21:54 +0200

    Landing page, login page and flags added TODO: reference to flag icon pack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
