485dfb6d632690137a1f9d3ed55489f2ab55f865
Bert de Boer <bertde.boer@hotmail.com>
2018-05-17 12:37:55 +0200

    create list done

--- src/app/create-word-list/create-word-list.component.ts
+++ src/app/create-word-list/create-word-list.component.ts
@@ -8,7 +8,7 @@ import * as $ from 'jquery';
 })
 export class CreateWordListComponent implements OnInit {
 
-	table: HTMLTableElement;
+	number = 5;
 
     constructor() { }
 
@@ -28,18 +28,26 @@ export class CreateWordListComponent implements OnInit {
 
 	createRows(){
 		for(let x = 0; x < 5; x ++){			
+			
 			var table = document.getElementById("input_field") as HTMLTableElement;
 			var i = table.getElementsByTagName("tr").length;
 			var row = table.insertRow(i);
+
 			var cell1 = row.insertCell(0);
-			cell1.innerHTML = "" + i;
-			
+			var newDiv = document.createElement("span");
+			newDiv.classList.add("input-group-text");
+			newDiv.id = "basic-addon1";
+			newDiv.innerHTML = "" + (i -1);
+			cell1.appendChild(newDiv);
+
 			var cell2 = row.insertCell(1);
 			var cell2_input = document.createElement("input");
+			cell2_input.classList.add("form-control");
 			cell2.appendChild(cell2_input);
 			
 			var cell3 = row.insertCell(2);
 			var cell3_input = document.createElement("input");
+			cell3_input.classList.add("form-control");
 			cell3.appendChild(cell3_input);
 
 		}
