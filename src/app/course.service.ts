37c7c3c1cdb07f52a1e2204eea913b537a9ed56d
TheElmo <jelmer.internet@xs4all.nl>
2018-06-06 11:27:45 +0200

    front-end for favorite

--- src/app/course.service.ts
+++ src/app/course.service.ts
@@ -29,4 +29,20 @@ export class CourseService {
   getUser(u_id: number) {
   	return this.http.get('http://localhost:8000/api/user/' + u_id + '/');
   }
+
+  addFavorite(u_id: number, c_id: number) {
+  	let favorite = {
+  		user: u_id,
+  		course: c_id
+  	};
+  	return this.http.post('http://localhost:8000/api/course/favorite', favorite, { headers: new HttpHeaders({ 'Content-Type': 'application/json' })});
+  }
+
+  delFavorite(u_id: number, c_id: number) {
+  	let favoriteToDel = {
+  		user: u_id,
+  		course: c_id
+  	};
+  	return this.http.post('http://localhost:8000/api/course/unfavorite', favoriteToDel { headers: new HttpHeaders({ 'Content-Type': 'application/json' })});
+  }
 }
