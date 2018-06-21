b4a242969f0bccf46ccbe89c004458506795c4b8
Bert de Boer <bertde.boer@hotmail.com>
2018-06-21 11:11:52 +0200

    merging

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HilangApiService } from './hilang-api.service';
+import { CookieService } from './cookie.service';
 
 @Injectable()
 
 export class AccountService {
 
-	constructor(private _api: HilangApiService) { }
+	constructor(private _api: HilangApiService, private _cookie: CookieService) { }
 
 	userInfoUrl = 'http://localhost:8000/api/user/1/';
 
+
+
 	getAccountSettings() {
-		return this._api.call('http://localhost:8000/api/user/1/', {});
+		let user_id = this._cookie.getValue()['user_id'];
+		return this._api.call('http://localhost:8000/api/user/' + user_id + '/', {});
 	}
 }
--- src/app/pre-exercise/pre-exercise.component.ts
+++ src/app/pre-exercise/pre-exercise.component.ts
@@ -10,6 +10,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class PreExerciseComponent implements OnInit {
 
 	lesson_id;
+	selectedOptions = {
+			random: false,
+			capital: false,
+			punctuation: false,
+			tillend: false
+		};
 
 	constructor(private _activatedRoute: ActivatedRoute, private router: Router) { }
 
@@ -22,14 +28,23 @@ export class PreExerciseComponent implements OnInit {
 	}
 
 	createExercise() {
-		var exercises = document.getElementsByName("exercise");
-		var selectedExercise;
+		var options = <NodeListOf<HTMLInputElement>>document.getElementsByName("options");
+		var exercises = <NodeListOf<HTMLInputElement>>document.getElementsByName("exercise");
+		var selectedExercise;		
+
+		for(var i = 0; i < options.length; i++){
+			if(options[i].checked){
+				console.log(options[i].value)
+				this.selectedOptions[options[i].value] = true;
+			}
+		}
 
 		for(var i = 0; i < exercises.length; i++){
 			if(exercises[i].checked){
-				selectedExercise = exercises[i].value
+				selectedExercise = exercises[i].value;
 			}
 		}
-		this.router.navigate(["user/lesson/" + this.lesson_id + "/" + selectedExercise])
+{/*		this.router.navigate(["user/lesson/" + this.lesson_id + "/" + selectedExercise]);
+*/}		console.log(this.selectedOptions);
 	}
 }
