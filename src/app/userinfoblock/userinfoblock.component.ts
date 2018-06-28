873e10b5f680489cfadea23c280f4f51341943a2
Bert de Boer <bertde.boer@hotmail.com>
2018-06-28 15:13:27 +0200

    merging

--- src/app/userinfoblock/userinfoblock.component.ts
+++ src/app/userinfoblock/userinfoblock.component.ts
@@ -10,30 +10,36 @@ import { CookieService } from '../cookie.service';
 })
 export class UserinfoblockComponent implements OnInit {
     user_id: number;
-    private user;
+    user = {
+            avatar: "",
+            name: "",
+            email: "";
+            bio: "",
+            type: "",
+            memberSince: "",
+        };
 
 
     constructor(private _api: HilangApiService, private _cookie: CookieService) { }
 
     ngOnInit() {
-        this.user = {
-            name: "Lang",
-            bio: "Here I can tell something about myself. This information is also shown in other places.",
-            avatar: "https://www.w3schools.com/w3css/img_lights.jpg",
-            memberSince: "May 18, 2018",
-            type: "Contributor"
-        }
+        
         this.user_id = this._cookie.getValue()['user_id'];
-        this._api.call('http://localhost:8000/api/user/' + this.user_id + "/", {}).subscribe(data => {
+        this._api.call('/user/' + this.user_id + "/", {}).subscribe(data => {
             console.log(data)
-            this.user.name = data.name;
-            this.user.email = data.email;
-            this.user.type = (data.distributor == 0) ? 'Normal user':'Distributor';
-            this.user.memberSince = data.created_at;
-            if(data.bio !== null){
-                this.user.bio = data.bio;
+            this.user['name'] = data['name'];
+            this.user['email'] = data['email'];
+            this.user['type'] = (data['distributor'] == 0) ? 'Normal user':'Distributor';
+            this.user['memberSince'] = data['created_at'];
+            if(data['bio'] !== null){
+                this.user['bio'] = data['bio'];
             } else {
-                this.user.bio = 'Empty bio';
+                this.user['bio'] = 'Empty bio';
+            }
+            if(data['avatar'] === '' || data['avatar'] === null ){
+                this.user['avatar'] = "../../assets/no-avatar.png" ;
+            } else {
+                this.user['avatar'] = data['avatar'];
             }
         });
     }
